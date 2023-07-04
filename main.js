f8f7dd5cc7b1f23363925e692bdf90e51ec71a5b
Барсуков Павел Сергеевич <pbarsukov@corp.local>
2023-07-04 17:18:40 +0300

    some fixes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const https = require("https");
 const fs = require("fs");
-const fileWrite = fs.createWriteStream("file.mp4");
-const fileRead = fs.createReadStream("file.mp4");
+const fileWriteMp4 = fs.createWriteStream("file.mp4");
+const fileWriteGif = fs.createWriteStream("file.gif");
 
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.API_KEY, {polling: true});
@@ -15,36 +15,43 @@ bot.on("message", async msg => {
   if (msg.text === "/start" || msg.text === "/restart") {
     await bot.sendMessage(chatId, "Привет! Этот бот нужен для того, чтобы скачивать видео из постов на пикабу. Чтобы начать, отправь мне ссылку на пост, а я заберу из него видео и отправлю тебе! Идет?");
   } else {
-    if (msg.text.includes("pikabu.ru/")) {
-      await bot.sendMessage(chatId, "Заглядываем в пост за ссылкой...");
-      const browser = await puppeteer.launch({ headless: "new", defaultViewport: null });
-      console.log("Браузер запущен")
-      const page = await browser.newPage();
-      console.log("Открыта новая страница")
-      await page.setUserAgent(userAgent.toString());
-      console.log("Задан юзер агент")
-      await page.goto(msg.text);
-      console.log("Мы попали на страницу")
-      await page.waitForSelector(".story-block_type_video .player__preview");
-      console.log("Мы нашли контейнер с видео")
-      const result = await page.evaluate(async () => {
-        console.log("Зашли в evaluate")
-        document.querySelector(".story-block_type_video").dispatchEvent(new Event("click"))
-        console.log("И кликнули на плей")
-        return document.querySelector(".player[data-type=\"video-file\"] .player__player video source[type=\"video/mp4\"]").getAttribute("src")
-      }).then(async data => {
-        await bot.sendMessage(chatId, "Скачиваем файл...");
-        https.get(data, async (resp) => {
-          await browser.close();
-          resp.pipe(fileWrite);
-          await bot.sendMessage(chatId, "Отправка");
-          await bot.sendVideo(chatId, fileRead, {}, {
-            contentType: "application/octet-stream"
-          }).then(async () => {
-            await bot.sendMessage(chatId, "Вот ваше видео");
+    if (/^https?:\/\/(?:www\.)?pikabu\.ru\/story\//.test(msg.text)) {
+      bot.sendMessage(chatId, "Заглядываем в пост за ссылкой...").then(async () => {
+        const browser = await puppeteer.launch({ headless: "new", defaultViewport: null });
+        const page = await browser.newPage();
+        await page.setUserAgent(userAgent.toString());
+        await page.goto(msg.text);
+        await page.waitForSelector(".app")
+          .then(() => page.evaluate(() => {
+            switch (document.querySelector(".player")?.dataset?.type) {
+              case "video-file": {
+                return document.querySelector(".player").dataset.source + ".mp4"
+              }
+              case "gifx": {
+                return document.querySelector(".player").dataset.source
+              }
+            }
+          }).then(data => {
+              if (data?.length) {
+                https.get(data, async (resp) => {
+                  await browser.close();
+                  if (data.indexOf(".mp4") !== -1) {
+                    resp.pipe(fileWriteMp4);
+                    bot.sendMessage(chatId, "Отправка").then(() => bot.sendVideo(chatId, "file.mp4"));
+                  } else {
+                    resp.pipe(fileWriteGif);
+                    bot.sendMessage(chatId, "Отправка").then(() => bot.sendAnimation(chatId, "file.gif"));
+                  }
+                })
+              } else {
+                bot.sendMessage(chatId, "Кажется, в посте нет видео! Попробуйте другой пост");
+              }
+            }
+          ))
+          .catch(() => {
+            bot.sendMessage(chatId, "Поста не существует! Попробуй другую ссылку")
           });
-        });
-      });
+      })
     } else {
       await bot.sendMessage(chatId, "Я могу работать только с постами с Пикабу");
     }
